4ec9f74efe15b13ec6c0d303c9de9ef26ac1221d
Brad Julian <brad.julian@resonate.com>
2021-10-29 16:45:28 -0400

    Complete section 4.18, "Factory Method"

---
+++ src/factory/Point.java
@@ -0,0 +1,33 @@
+package factory;
+
+import static factory.CoordinateSystem.POLAR;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+
+public class Point {
+
+  private double x, y;
+
+  private Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  private Point(double a, double b, CoordinateSystem cs) {
+    if (cs == POLAR) {
+      x = a * cos(b);
+      y = a * sin(b);
+    } else {
+      x = a;
+      y = b;
+    }
+  }
+
+  public static Point newCartesianPoint(double x, double y) {
+    return new Point(x, y);
+  }
+
+  public static Point newPolarPoint(double rho, double theta) {
+    return new Point(rho * cos(theta), rho * sin(theta));
+  }
+}
