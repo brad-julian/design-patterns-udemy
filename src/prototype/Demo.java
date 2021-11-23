668cf245bba52b2a8548e0bbdc04f9e7f923109f
Brad Julian <brad.julian@resonate.com>
2021-11-23 15:37:39 -0500

    Complete section 5.23, "Don't Use Cloneable"

---
+++ src/prototype/Demo.java
@@ -0,0 +1,15 @@
+package prototype;
+
+public class Demo {
+
+  public static void main(String[] args) throws CloneNotSupportedException {
+    Person johnSmith = new Person(new Address("London Road", 123), new String[]{"John", "Smith"});
+    System.out.println(johnSmith);
+
+    Person janeSmith = (Person) johnSmith.clone();
+    janeSmith.names[0] = "Jane";
+    janeSmith.address.houseNumber = 124;
+    System.out.println(janeSmith);
+    System.out.println(johnSmith);
+  }
+}
---
+++ src/prototype/Person.java
@@ -0,0 +1,27 @@
+package prototype;
+
+import java.util.Arrays;
+
+public class Person {
+
+  public Address address;
+  public String[] names;
+
+  public Person(Address address, String[] names) {
+    this.address = address;
+    this.names = names;
+  }
+
+  @Override
+  public String toString() {
+    return "Person{" +
+        "address=" + address +
+        ", names=" + Arrays.toString(names) +
+        '}';
+  }
+
+  @Override
+  protected Object clone() throws CloneNotSupportedException {
+    return new Person((Address) address.clone(), names.clone());
+  }
+}
