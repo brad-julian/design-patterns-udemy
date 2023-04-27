4c9240bfa75c22ebbbf3e928dbf4241768a1e35e
Brad Julian <brad.julian@resonate.com>
2023-04-27 15:31:32 -0400

    Complete exercise 7, "Composite Coding Exercise"

---
+++ src/composite/exercise.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.util.function.Consumer;
+
+interface ValueContainer extends Iterable<Integer> {}
+
+class SingleValue implements ValueContainer
+{
+  public int value;
+
+  // please leave this constructor as-is
+  public SingleValue(int value)
+  {
+    this.value = value;
+  }
+
+  @Override
+  public Iterator<Integer> iterator() {
+    return Collections.singleton(value).iterator();
+  }
+
+  @Override
+  public void forEach(Consumer<? super Integer> action) {
+    action.accept(value);
+  }
+
+  @Override
+  public Spliterator<Integer> spliterator() {
+    return Collections.singleton(value).spliterator();
+  }
+}
+
+class ManyValues extends ArrayList<Integer> implements ValueContainer {
+}
+
+
+class MyList extends ArrayList<ValueContainer>
+{
+  // please leave this constructor as-is
+  public MyList(Collection<? extends ValueContainer> c)
+  {
+    super(c);
+  }
+
+  public int sum()
+  {
+    int sum = 0;
+    for (ValueContainer valCon : this) {
+      for (Integer value : valCon) {
+        sum += value;
+      }
+    }
+    return sum;
+  }
+}
