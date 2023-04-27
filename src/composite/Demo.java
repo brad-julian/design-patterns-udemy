9fd490bb4b9a0ad7f07f76b850c6d55c4c9efe9b
Brad Julian <brad.julian@resonate.com>
2023-04-27 15:13:07 -0400

    Complete section 9.47, "Geometric Shapes"

---
+++ src/composite/Demo.java
@@ -0,0 +1,18 @@
+package composite;
+
+public class Demo {
+
+  public static void main(String[] args) {
+    GraphicObject drawing = new GraphicObject();
+    drawing.setName("My Drawing");
+    drawing.children.add(new Square("Red"));
+    drawing.children.add(new Circle("Yellow"));
+
+    GraphicObject group = new GraphicObject();
+    group.children.add(new Circle("Blue"));
+    group.children.add(new Circle("Blue"));
+    drawing.children.add(group);
+
+    System.out.println(drawing);
+  }
+}
---
+++ src/composite/GraphicObject.java
@@ -0,0 +1,56 @@
+package composite;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static java.lang.String.join;
+import static java.lang.System.lineSeparator;
+import static java.util.Collections.nCopies;
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
+public class GraphicObject {
+
+  String name = "Group";
+  String color;
+  List<GraphicObject> children = new ArrayList<>();
+
+  public GraphicObject() {
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    print(sb, 0);
+    return sb.toString();
+  }
+
+  private void print(StringBuilder sb, int depth) {
+    sb.append(join("", nCopies(depth, "*")))
+        .append(depth > 0 ? " " : "")
+        .append(isBlank(color) ? "" : color + " ")
+        .append(getName())
+        .append(lineSeparator());
+    for (GraphicObject child : children) {
+      child.print(sb, depth + 1);
+    }
+  }
+}
